27183040b3ba0227e7f048b2e6e314832ca17291
VoronaPavel <vorona.pavel.nickolaevich@gmail.com>
2015-10-12 21:37:51 +0200

    polishing

--- src/LuckyScala.scala
+++ src/LuckyScala.scala
@@ -1,13 +1,13 @@
 object LuckyScala extends LuckyTicket {
 
   def isLucky(number: String) = {
-    val sumOfDigits = (number: String) => number.map(_.asDigit).sum
-    val (left, right) = number.splitAt(number.length / 2)
+    val sumOfDigits = (number: String) => number map(_ asDigit) sum
+    val (left, right) = number splitAt(number.length / 2)
     sumOfDigits(left) == sumOfDigits(right)
   }
 
   def countLucky(min: String, max: String) =
-    Range.BigInt.inclusive(BigInt(min), BigInt(max), 1).count(element => isLucky(element toString))
+    Range.BigInt.inclusive(BigInt(min), BigInt(max), 1).count(e => isLucky(e toString))
 
   def countLucky(min: Long, max: Long) = countLucky(min toString, max toString)
 }
