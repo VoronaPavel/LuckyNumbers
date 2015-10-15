da0abb1d792049f6441f6e650139c948bf322c6a
P.Vorona <p.vorona@itransition.com>
2015-10-15 13:06:40 +0300

    polishing

--- src/LuckyScala.scala
+++ src/LuckyScala.scala
@@ -1,8 +1,8 @@
 object LuckyScala extends LuckyTicket {
 
   def isLucky(number: String) = {
-    val sumOfDigits = (number: String) => number map(_ asDigit) sum
-    val (left, right) = number splitAt(number.length / 2)
+    val sumOfDigits = (number: String) => number map (_ asDigit) sum
+    val (left, right) = number splitAt number.length / 2
     sumOfDigits(left) == sumOfDigits(right)
   }
 
